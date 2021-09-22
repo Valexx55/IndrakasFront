f0e4085465499f7e6b04dbb161525bd66d35c0b0
valexx55 <valexx55@gmail.com>
2021-09-22 13:30:04 +0200

    my app completo

--- my-app/src/app/componentes/imc/imc.component.ts
+++ my-app/src/app/componentes/imc/imc.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Imc } from 'src/app/imc';
+import { Tipoimc } from 'src/app/tipoimc';
 
 @Component({
   selector: 'app-imc',
@@ -18,6 +19,7 @@ export class ImcComponent implements OnInit {
 
   public titulo:string = "CÁLCULO DEL IMC";
   public oimc: Imc;
+  public array_imc : Array<Imc>;
   private static readonly FOTO_DESNUTRIDO: string = "assets/desnutrido.jfif";
   private static readonly FOTO_DELGADO: string = "assets/delgado.jfif";
   private static readonly FOTO_IDEAL: string = "assets/ideal.jfif";
@@ -28,6 +30,7 @@ export class ImcComponent implements OnInit {
   constructor() {
 
     this.oimc = new Imc();
+    this.array_imc = new Array<Imc>();
     /*this.peso= 0;
     this.altura = 0;
     this.imc = 0;
@@ -39,7 +42,35 @@ export class ImcComponent implements OnInit {
   }
 
 
-  calcularImc() {
+  mostrarArray ()
+  {
+    for (let eimc of this.array_imc)
+    {
+      console.log(`ALTURA ${eimc.altura}`);
+      console.log(`PESO ${eimc.peso}`);
+      console.log(`IMC num ${eimc.numerico}`);
+      console.log(`IMC nom ${eimc.nominal}`);
+      console.log(`FOTO ${eimc.foto}`);
+    }
+  }
+
+  nuevoItemArray (oimc:Imc) : Imc
+  {
+    let imc_aux: Imc;
+
+      imc_aux = new Imc();
+      imc_aux.altura = oimc.altura;
+      imc_aux.peso = oimc.peso;
+      imc_aux.foto = oimc.foto;
+      imc_aux.nominal = oimc.nominal;
+      imc_aux.numerico = oimc.numerico;
+      imc_aux.lectura = oimc.lectura;
+    
+    return imc_aux;
+  }
+
+  calcularImc() { 
+
     if (this.oimc.peso != 0 && this.oimc.altura != 0) {
       const imcx = (this.oimc.peso / (this.oimc.altura * this.oimc.altura));
       this.oimc.numerico = imcx;
@@ -48,27 +79,74 @@ export class ImcComponent implements OnInit {
       if (imcx < 16) {
         this.oimc.lectura = "el cejas te puede";
         this.oimc.foto = ImcComponent.FOTO_DESNUTRIDO;
+        this.oimc.nominal = Tipoimc.DESNUTRIDO;
       }
       else if (imcx >= 16 && imcx < 18) {
         this.oimc.lectura = "el cejas no te puede";
         this.oimc.foto = ImcComponent.FOTO_DELGADO;
+        this.oimc.nominal = Tipoimc.DELGADO;
       }
       else if (imcx >= 18 && imcx < 25) {
         this.oimc.lectura = "no te vendria mal comer mas fruta";
         this.oimc.foto = ImcComponent.FOTO_IDEAL;
+        this.oimc.nominal = Tipoimc.IDEAL;
       }
       else if (imcx >= 25 && imcx < 31) {
         this.oimc.lectura = "tas gordito eh";
         this.oimc.foto = ImcComponent.FOTO_SOBREPESO;
+        this.oimc.nominal = Tipoimc.SOBREPESO;
       }
       else if (imcx > 31) {
         this.oimc.lectura = "menos phoskitos ibai";
         this.oimc.foto = ImcComponent.FOTO_OBESO;
+        this.oimc.nominal = Tipoimc.OBESO;
       }
+
+      //let imc_aux :Imc = this.nuevoItemArray (this.oimc);
+      let imc_aux = this.nuevoItemArray (this.oimc);
+  
+      this.array_imc.push(imc_aux);
+      console.log(`ARRAY DE IMCS = ${this.array_imc}`);
+      this.mostrarArray();    
+
+
 
     } else {
       alert("Debes ingresar peso y altura, enga q no te de bergÜensita.")
     }
   }
+  ordenarPorPeso ()
+  {
+    console.log("Ordenando por peso...");
+    this.array_imc.sort (
+      function (a, b) {
+        //si a es mayor, devuelvo +
+        //si b es mayor, devuelvo -
+        //si son iguales, devuelvo 0
+        return (a.peso-b.peso);
+      }
+    )
+  }
 
+  ordenarPorAlutra ()
+  {
+    console.log("Ordenando por altura...");
+    this.array_imc.sort (
+      function (a, b) {
+        //si a es mayor, devuelvo +
+        //si b es mayor, devuelvo -
+        //si son iguales, devuelvo 0
+        return (a.altura-b.altura);
+      }
+    )
+  }
+
+  limpiar ()
+  {
+    console.log("Limpiando...");
+    //this.array_imc = new Array<Imc>();
+    this.array_imc.length = 0;
+
+
+  }
 }
